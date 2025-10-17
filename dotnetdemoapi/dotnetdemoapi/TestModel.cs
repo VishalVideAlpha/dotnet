66edccedd3e5e8e5f094b6ff1637c345b4102a9a
VishalVideAlpha <vishal@videalpha.com>
2025-10-17 17:22:11 +0530

    Create TestModel.cs

---
+++ dotnetdemoapi/dotnetdemoapi/TestModel.cs
@@ -0,0 +1,11 @@
+namespace dotnetdemoapi
+{
+    public class TestModel
+    {
+        public DateOnly Date { get; set; }
+
+        public int number { get; set; }
+
+        public string? name { get; set; }
+    }
+}
