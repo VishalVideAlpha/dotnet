977266b119ae7653bceb62b6823c724cbef5df1a
VishalVideAlpha <vishal@videalpha.com>
2025-10-17 17:05:19 +0530

    Update WeatherForecastController.cs

--- dotnetdemoapi/dotnetdemoapi/Controllers/WeatherForecastController.cs
+++ dotnetdemoapi/dotnetdemoapi/Controllers/WeatherForecastController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace dotnetdemoapi.Controllers
 {
     [ApiController]
-    [Route("[manisha]")]
+    [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
