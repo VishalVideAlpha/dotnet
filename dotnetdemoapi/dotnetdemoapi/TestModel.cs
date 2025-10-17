d150342c5bf03c0b17358e3050bf7e3115542514
VishalVideAlpha <vishal@videalpha.com>
2025-10-17 17:46:48 +0530

    Update TestModel.cs

--- dotnetdemoapi/dotnetdemoapi/TestModel.cs
+++ dotnetdemoapi/dotnetdemoapi/TestModel.cs
@@ -2,8 +2,7 @@ namespace dotnetdemoapi
 {
     public class TestModel
     {
-        public DateOnly Date { get; set; }
-
+      
         public int number { get; set; }
 
         public string? name { get; set; }
